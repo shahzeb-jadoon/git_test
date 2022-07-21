22861f5877bc8cec945253072ae615c4c512fc4c
Shahzeb Jadoon <shahzeb.jadoon.sj@gmail.com>
2022-07-21 00:02:00 -0500

    Un-comment prompt and log statements

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ function add7(number) {
     return number + 7;
 }
 
-//let num1 = Number(prompt("What number do you want to add 7 to?", 0))
+let num1 = Number(prompt("What number do you want to add 7 to?", 0))
 
-//console.log(add7(num1));
+console.log(add7(num1));
 
 function multiply(num1, num2) {
     return num1 * num2;
 }
 
-//let num2 = Number(prompt("What number do you want to multiply the first number with?", 0))
+let num2 = Number(prompt("What number do you want to multiply the first number with?", 0))
 
-//console.log(multiply(num1, num2));
+console.log(multiply(num1, num2));
 
 function capitalize(text) {
     if (typeof text === 'string' || text instanceof String) {
@@ -24,7 +24,7 @@ function capitalize(text) {
 
 let text = prompt("What text do you want capitalized?");
 
-//console.log(capitalize(text));
+console.log(capitalize(text));
 
 function lastLetter (text) {
     return text.slice(-1);
