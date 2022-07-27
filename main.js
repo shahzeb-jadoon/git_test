2d66dd6bbce71bbff46a3c6d866f83a8f7a6f963
Shahzeb Jadoon <shahzeb.jadoon.sj@gmail.com>
2022-07-26 22:26:18 -0500

    Add comments to describe function purpose

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function add7(number) {
+function add7(number) { // Add 7 to the provided number
     return number + 7;
 }
 
@@ -6,7 +6,7 @@ let num1 = Number(prompt("What number do you want to add 7 to?", 0))
 
 console.log(add7(num1));
 
-function multiply(num1, num2) {
+function multiply(num1, num2) { // Multiply two numbers
     return num1 * num2;
 }
 
@@ -14,7 +14,7 @@ let num2 = Number(prompt("What number do you want to multiply the first number w
 
 console.log(multiply(num1, num2));
 
-function capitalize(text) {
+function capitalize(text) { // Capitalize provided text
     if (typeof text === 'string' || text instanceof String) {
         return (text)[0].toUpperCase() + (text).slice(1).toLowerCase();
     } else {
@@ -26,7 +26,7 @@ let text = prompt("What text do you want capitalized?");
 
 console.log(capitalize(text));
 
-function lastLetter (text) {
+function lastLetter (text) { // Return last letter from string
     return text.slice(-1);
 }
 
